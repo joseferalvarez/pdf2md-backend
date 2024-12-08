a9b22d9e7a660e53e8d5924d4fc9be7ae40546da
Jose Fernando Álvarez Romero <joseferalvarezromero@gmail.com>
2024-12-08 11:16:21 +0100

    env variable added and file types control added

--- app/main.py
+++ app/main.py
@@ -1,21 +1,47 @@
 from fastapi import FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from fastapi.concurrency import run_in_threadpool
-import os
 import tempfile
 import pymupdf4llm
 import requests
+import os
 
 app = FastAPI()
+
+api_env = os.getenv("API_ENV", "DEV")
+
+origins = [
+  "http://127.0.0.1:8000",
+  "http://localhost:8000",
+]
+
+if api_env == "PROD":
+  origins = ["https://pdf2md.joseferalvarez.dev"]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=False,
+  allow_methods=["POST"],
+  allow_headers=["*"]
+)
 
 class BaseURL(BaseModel):
   url: str
 
-@app.post("/api/v1/file/")
+@app.post("/v1/file/")
 async def upload_file(file: UploadFile):
   file_bytes = await file.read()
 
-  with tempfile.NamedTemporaryFile(delete=True, mode="w+b") as temp_file:
+  if len(file.filename.split(".")) < 2 or file.filename.split(".")[1] != "pdf":
+      return {"file": "You can only upload pdf files"}
+
+  with tempfile.NamedTemporaryFile(delete=True, mode="w+b", suffix="pdf") as temp_file:
+
+    if not temp_file or not temp_file.name:
+      return {"file": "The file can not be converted"}
+    
     temp_file.write(file_bytes)
 
     md_file = await run_in_threadpool(pymupdf4llm.to_markdown, temp_file.name)
@@ -23,12 +49,16 @@ async def upload_file(file: UploadFile):
   
   return {"file": "The file can not be converted"}
 
-@app.post("/api/v1/url/")
+@app.post("/v1/url/")
 async def download_file(req: BaseURL):
   res = requests.get(req.url)
   pdf_content = res.content
 
-  with tempfile.NamedTemporaryFile(delete=True, mode="w+b", suffix=".pdf") as temp_file:
+  with tempfile.NamedTemporaryFile(delete=True, mode="w+b", suffix="pdf") as temp_file:
+
+    if not temp_file or not temp_file.name:
+      return {"file": "The file can not be converted"}
+    
     temp_file.write(pdf_content)
 
     md_file = await run_in_threadpool(pymupdf4llm.to_markdown, temp_file.name)
